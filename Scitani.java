b099cf4282219c48ce417b0201fd8500aadd1623
FallingLeaf79 <44774181+FallingLeaf79@users.noreply.github.com>
2018-12-03 08:43:37 +0100

    Added Scitani.java
    Prints 10 random addition problems with values between 0 and 20.

---
+++ Scitani.java
@@ -0,0 +1,18 @@
+/* This program prints 10 random addition problems with values between 0 and 20.
+ */
+public class Scitani {
+  public static void main(String[] args) {
+    int problems = 10;
+    int max = 20;
+    int a;
+    int b;
+    java.util.Random random = new java.util.Random();
+
+    while (problems > 0) {
+      a = random.nextInt(max);
+      b = random.nextInt(max);
+      System.out.println(a + " + " + b + " = ?");
+      problems--;
+    }
+  }
+}
