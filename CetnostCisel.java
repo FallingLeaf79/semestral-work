ad29d29c143eda3f9c74ed846a4869d51e7b7bda
FallingLeaf79 <44774181+FallingLeaf79@users.noreply.github.com>
2018-12-07 20:45:21 +0100

    Methods are your friend
    More code readabilty measures.

--- CetnostCisel.java
+++ CetnostCisel.java
@@ -8,6 +8,33 @@ public class CetnostCisel {
     }
   }
 
+  public static boolean addIfPresent(int[][] array, int input) {
+    for (int x = 0; x < (array[0].length); x++) {
+      if (input == array[0][x]) {
+        array[1][x]++;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static int[][] makeBigger(int[][] array) {
+    int[][] helperArray = new int[2][array[1].length + 1];
+    copyFreqArray(array, helperArray);
+
+    array = new int[2][helperArray[1].length];
+    copyFreqArray(helperArray, array);
+
+    return array;
+  }
+
+  public static void printMostFrequent(int[][] frequency) {
+    System.out.println(
+      frequency[0][getIndexOfLargest(frequency[1])] +
+      "\t" + frequency[1][getIndexOfLargest(frequency[1])] + "x"
+    );
+  }
+
   public static int getIndexOfLargest(int[] array) {
     int largest = 0;
     for (int i = 0; i < array.length; i++) {
@@ -21,7 +48,6 @@ public class CetnostCisel {
   public static void main(String[] args) {
     java.util.Scanner sc = new java.util.Scanner(System.in);
     int[][] frequency = new int[2][1];
-    int[][] extendFrequency;
     int numberOfValues = 0;
 
     while (sc.hasNextInt()) {
@@ -34,34 +60,19 @@ public class CetnostCisel {
         frequency[1][0] = 1;
         alreadyPresent = true;
       }
-
-      else for (int x = 0; x < (frequency[0].length); x++) {
-        if (input == frequency[0][x]) {
-          frequency[1][x]++;
-          alreadyPresent = true;
-        }
+      else {
+        alreadyPresent = addIfPresent(frequency, input);
       }
 
       if (!alreadyPresent) {
-        extendFrequency = new int[2][frequency[1].length + 1];
-        copyFreqArray(frequency, extendFrequency);
-
-        frequency = new int[2][extendFrequency[1].length];
-        copyFreqArray(extendFrequency, frequency);
-
+        frequency = makeBigger(frequency);
         frequency[0][frequency[0].length - 1] = input;
         frequency[1][frequency[1].length - 1] = 1;
       }
     }
 
-    /* Prints the numbers and their frequency, in order of their frequency.
-     * (Most frequent values appear at the top)
-     */
     for (int z = 0; z < frequency[0].length; z++) {
-      System.out.println(
-        frequency[0][getIndexOfLargest(frequency[1])] +
-        "\t" + frequency[1][getIndexOfLargest(frequency[1])] + "x"
-      );
+      printMostFrequent(frequency);
       // discarding an already printed value
       frequency[1][getIndexOfLargest(frequency[1])] = 0;
     }
