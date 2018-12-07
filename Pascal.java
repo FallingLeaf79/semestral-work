5a4d153caa68027df613adb82233505d1e347451
FallingLeaf79 <44774181+FallingLeaf79@users.noreply.github.com>
2018-12-07 21:26:04 +0100

    Cleanup

--- Pascal.java
+++ Pascal.java
@@ -4,26 +4,23 @@
 public class Pascal {
   public static void main(String[] args) {
     int a = Integer.parseInt(args[0]);
-    /* A two-dimensional array is created, which is then used to make
-     * a table, containing the values of a Pascal's triangle.
-     * Those are then printed.
-     */
-    int triangle[][] = new int[a][a];
+    int pascal[][] = new int[a][a];
+
     for (int x = 0; x < a; x++) {
-      triangle[x][0] = 1;
+      pascal[x][0] = 1;
       if (x >= 1) {
         for (int y = 1; y < a; y++) {
-          if (triangle[x-1][y-1] == 0) {
-            continue;                     //for better efficiency
+          if (pascal[x - 1][y - 1] == 0) {
+            continue;
           }
-          triangle[x][y] = (triangle[x-1][(y-1)]) + (triangle [x-1][y]);
+          pascal[x][y] = (pascal[x - 1][y - 1]) + (pascal [x - 1][y]);
         }
       }
       for (int z = 0; z <= x; z++) {
-        for (int tab = (a-(x+1)); tab > 0 && z < 1; tab--){
+        for (int tab = (a - (x + 1)); tab > 0 && z < 1; tab--){
           System.out.printf("\t");
         }
-        System.out.printf("%d\t\t", triangle[x][z]);
+        System.out.printf("%d\t\t", pascal[x][z]);
       }
       System.out.println();
     }
